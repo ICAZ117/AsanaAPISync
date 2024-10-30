d27c62e31305a6270d2dfdbd1e06c066e3183786
ICAZ117 <ibraheemcazalas@gmail.com>
2024-10-30 11:38:08 -0400

    Loaded API key

--- AsanaAPISync.js
+++ AsanaAPISync.js
@@ -1,5 +1,9 @@
+require('dotenv').config();
+
 function main() {
-    // Asana
+    // Get API key from local .env file
+    const apiKey = process.env.API_KEY;
+    console.log(apiKey);
 }
 
 main();
