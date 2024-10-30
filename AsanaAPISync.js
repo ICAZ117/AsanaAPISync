d073cea0f8c6243a9c18686ab5c811f8bcd63128
ICAZ117 <ibraheemcazalas@gmail.com>
2024-10-30 12:39:05 -0400

    Added code to parse excel file

--- AsanaAPISync.js
+++ AsanaAPISync.js
@@ -1,4 +1,6 @@
 require('dotenv').config();
+const reader = require('xlsx');
+const file = reader.readFile('Workbook.xlsx');
 
 const doDebug = true;
 
@@ -41,7 +43,34 @@ async function main() {
     const apiKey = process.env.API_KEY;
     debug("API Key", apiKey);
 
-    await createComment(1207196044602694, "AsanaAPISync_v0.0.1alpha: API Test", apiKey);
+    const targetSheet = "SheetB";
+    const sheets = file.SheetNames;
+
+    // Iterate through all sheets in the workbook
+    for (let i = 0; i < sheets.length; i++) {
+        // Check if the current sheet is the target sheet
+        if (sheets[i] === targetSheet) {
+            debug("Sheet", sheets[i]);
+            const temp = reader.utils.sheet_to_json(
+                file.Sheets[file.SheetNames[i]]
+            );
+
+            // Iterate through each row
+            for (let j = 0; j < temp.length; j++) {
+                debug("Row", temp[j]);
+
+                // Get project, date, and notes from the row
+                const project = temp[j].Project;
+                const date = temp[j].Date;
+                const notes = temp[j].Notes;
+
+                debug("Project", project);
+                debug("Date", date);
+                debug("Notes", notes);
+                debug("NEXT ROW\n\n");
+            }
+        }
+    }
 }
 
 main();
